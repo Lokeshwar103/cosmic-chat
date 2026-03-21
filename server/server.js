ee5a0a277d036c76ccd89b40a6f70ffb0bde4fe4
Lokeshwar Bantu <lokeshwarbantu136@gmail.com>
2026-03-21 23:58:00 +0530

    fix email smtp

--- server/server.js
+++ server/server.js
@@ -52,10 +52,12 @@ mongoose
 // EMAIL (FIXED)
 // =======================
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS // MUST be app password
+    pass: process.env.EMAIL_PASS
   }
 });
 
