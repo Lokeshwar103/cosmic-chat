5f300a3dca2d83b459cb40d7b338cc445669862b
Lokeshwar Bantu <lokeshwarbantu136@gmail.com>
2026-03-22 13:20:15 +0530

    Fixed port binding issue

--- server/server.js
+++ server/server.js
@@ -235,6 +235,6 @@ io.on("connection", socket => {
 // =======================
 // START
 // =======================
-server.listen(PORT, () => {
+server.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
